9233a5e04c1985403a7e9fb44a03f273ce2763f0
jersonsilvaa <jerson.silva99@hotmail.com>
2022-08-10 06:50:55 -0500

    #20 [Cambios en el index.php]

--- index.php
+++ index.php
@@ -13,29 +13,29 @@
     <div class="container">
         <section class="form signup">
             <header>Chat app online</header>
-            <form action="#" enctype="multipart/form-data" >
+            <form action="#" enctype="multipart/form-data" autocomplete="off" >
                 <div class="error-txt"></div>
                 <div class="name-details">
                     <div class="field input">
                         <label>Nombres</label>
-                        <input type="text" name="fname" placeholder="Nombres" require >
+                        <input type="text" name="fname" placeholder="Nombres" required >
                     </div>
                     <div class="field input">
                         <label>Apellidos</label>
-                        <input type="text" name="lname" placeholder="Apellidos" require >
+                        <input type="text" name="lname" placeholder="Apellidos" required >
                 </div>
                 <div class="field input">
                     <label>Correo</label>
-                    <input type="text" name="email" placeholder="Correo" require >
+                    <input type="text" name="email" placeholder="Correo" required >
                 </div>
                 <div class="field input">
                     <label>Contraseña</label>
-                    <input type="password" name="password" placeholder="Contraseña" require >
+                    <input type="password" name="password" placeholder="Contraseña" required >
                     <i class="fas fa-eye"></i>
                 </div>
                 <div class="field image">
                     <label>Seleccionar imagen de perfil</label>
-                    <input type="file" name="image" require >
+                    <input type="file" name="image" required >
                 </div>
                 <div class="field button">
                     <input type="submit" value="Continuar">
