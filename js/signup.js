9572a96edbfdd474c47d642313f64c0d420a63fc
jersonsilvaa <jerson.silva99@hotmail.com>
2022-08-09 19:44:09 -0500

    #16 [Enviando respuesta al tener algún error en el formulario]

---
+++ js/signup.js
@@ -0,0 +1,29 @@
+const form = document.querySelector(".signup form"),
+continueBtn = form.querySelector(".button input"),
+errorTxt = form.querySelector(".error-txt")
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+}
+
+continueBtn.onclick = () => {
+    let xhr = new XMLHttpRequest()
+    xhr.open("POST", "php/signup.php", true);
+
+    xhr.onload = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                
+                if (data == "success") {
+                    
+                } else {
+                    errorTxt.textContent = data
+                    errorTxt.style.display = "block"
+                }
+            }
+        }
+    }
+    let formData = new FormData(form)
+    xhr.send(formData)
+}
