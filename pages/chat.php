578f1e5a2216e070e390ee87891ca2a61a7c3fa9
jersonsilvaa <jerson.silva99@hotmail.com>
2022-08-10 11:26:13 -0500

    #23 [Usando php para el header]

--- pages/chat.php
+++ pages/chat.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/2936/2936956.png" type="image/x-icon">
-    <link rel="stylesheet" href="../css/styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
-    <title>Chat app online</title>
-</head>
+<?php 
+    include_once "header.php";
+?>
 <body>
     <div class="container">
         <section class="chat-area">
