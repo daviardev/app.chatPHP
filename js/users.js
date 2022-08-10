90df6d36627dad460dbf09c3accf9ca12c0df102
jersonsilvaa <jerson.silva99@hotmail.com>
2022-08-10 18:18:03 -0500

    #31 [Busqueda de usuarios en la lista]

--- js/users.js
+++ js/users.js
@@ -6,6 +6,32 @@ searchBtn.onclick = () => {
     searchBar.classList.toggle('active')
     searchBar.focus()
     searchBtn.classList.toggle('active')
+    searchBar.value = ""
+}
+
+//Funcionabilidad de la barra de busqueda
+searchBar.onkeyup = () => {
+    let searchTerm = searchBar.value
+
+    if (searchTerm != "") {
+        searchBar.classList.add("active")
+    } else {
+        searchBar.classList.remove("active")
+    }
+
+    let xhr = new XMLHttpRequest()
+    xhr.open("POST", "../php/search.php", true)
+
+    xhr.onload = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response
+                userList.innerHTML = data
+            }
+        }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+    xhr.send("searchTerm=" + searchTerm)
 }
 
 setInterval(() => {
@@ -16,7 +42,10 @@ setInterval(() => {
         if(xhr.readyState === XMLHttpRequest.DONE){
             if(xhr.status === 200){
                 let data = xhr.response
-                userList.innerHTML = data
+                
+                if (!searchBar.classList.contains("active")) {
+                    userList.innerHTML = data
+                }
             }
         }
     }
