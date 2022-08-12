659ac8744cb33b687a7336b70a4460b2451a6bd3
jersonsilvaa <jerson.silva99@hotmail.com>
2022-08-12 11:40:00 -0500

    #51 [Cambiar estado de desconectado a conectado cuando el usuario se loguea]

--- php/login.php
+++ php/login.php
@@ -9,8 +9,13 @@
 
         if (mysqli_num_rows($sql) > 0) {
             $row = mysqli_fetch_assoc($sql);
-            $_SESSION['unique_id'] = $row['unique_id'];
-            echo "success";
+            $status = "Conectado";
+            $sql2 = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}");
+            
+            if ($sql2) {
+                $_SESSION['unique_id'] = $row['unique_id'];
+                echo "success";
+            }
         } else {
             echo "Correo o contraseña incorrecto.";
         }
