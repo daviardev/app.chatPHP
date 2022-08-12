a14b3d8376ec5367da9c0710b78902131f2cc3f8
jersonsilvaa <jerson.silva99@hotmail.com>
2022-08-12 11:38:21 -0500

    #48 [Cambio de ruta]

--- pages/users.php
+++ pages/users.php
@@ -26,7 +26,7 @@
                         <p><?php echo $row['status'] ?></p>
                     </div>
                 </div>
-                <a href="login.php" class="logout">Cerrar sesión</a>
+                <a href="../php/logout.php?logout_id=<?php echo $row['unique_id'] ?>" class="logout">Cerrar sesión</a>
             </header>
             <div class="search">
                 <span class="text">Seleccionar un usuario para comenzar a charlar o búscalo</span>
