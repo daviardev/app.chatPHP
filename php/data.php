7e2c938791a2cb4d9618a8d4a94393da0aa3be9f
jersonsilvaa <jerson.silva99@hotmail.com>
2022-08-12 07:12:14 -0500

    #41 [Mostrando puntos a partir de 28 caracteres de un mensaje y mostrando cuando tu envias un mensaje a otra persona]

--- php/data.php
+++ php/data.php
@@ -1,12 +1,28 @@
 <?php 
     while ($row = mysqli_fetch_assoc($sql)) {
+        $sql2 = "SELECT * FROM message WHERE (incoming_msg_id = {$row['unique_id']}
+                OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id}
+                OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
+
+        $query2 = mysqli_query($conn, $sql2);
+        $row2 = mysqli_fetch_assoc($query2);
+
+        if (mysqli_num_rows($query2) > 0) {
+            $result = $row2['msg'];
+        } else {
+            $result = "Los mensajes no están disponibles.";
+        }
+        
+        (strlen($result) > 28) ? $msg = substr($result, 0, 28).'...' : $msg = $result;
+        ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "Tu: " : $you = "";
+
         $output .= '
         <a href="chat.php?user_id='.$row['unique_id'].'">
             <div class="content">
                 <img src="../php/images/'.$row['img'].'" alt="">
                 <div class="details">
                 <span>'. $row['fname']. " ". $row['lname'] .'</span>
-                <p>Te apetece grabar?</p>
+                <p>'. $you . $msg .'</p>
                 </div>
             </div>
                 <div class="status-dot"><i class="fas fa-circle"></i></div>
