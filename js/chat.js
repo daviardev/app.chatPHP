7cfdc53d6557094dd368e94c89992fc84e68038f
jersonsilvaa <jerson.silva99@hotmail.com>
2022-08-11 21:40:10 -0500

    #38 [Limpiar campo de texto al enviar mensaje]

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,7 @@
 const form = document.querySelector(".typing-area"),
 inputField = form.querySelector(".input-field"),
-sendBtn = form.querySelector("button")
+sendBtn = form.querySelector("button"),
+chatBox = document.querySelector(".chat-box")
 
 form.onsubmit = (event) => {
     event.preventDefault()
@@ -20,3 +21,18 @@ sendBtn.onclick = () => {
     let formData = new FormData(form)
     xhr.send(formData)
 }
+setInterval(() => {
+    let xhr = new XMLHttpRequest()
+    xhr.open("POST", "../php/get-chat.php", true)
+
+    xhr.onload = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response
+                chatBox.innerHTML = data
+            }
+        }
+    }
+    let formData = new FormData(form)
+    xhr.send(formData)
+}, 500)
