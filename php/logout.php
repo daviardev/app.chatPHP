978cdf168473a93ef91c36a933c628798ad53c7d
jersonsilvaa <jerson.silva99@hotmail.com>
2022-08-12 11:40:38 -0500

    #52 [Cambiar a desconectado cuando se desloguea]

---
+++ php/logout.php
@@ -0,0 +1,23 @@
+<?php 
+    include_once "config.php";
+    session_start();
+    
+    if (isset($_SESSION['unique_id'])) {
+        $logout_id = mysqli_real_escape_string($conn, $_GET['logout_id']);
+
+        if (isset($logout_id)) {
+            $status = "Desconectado";
+            $sql = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$logout_id}");
+
+            if ($sql) {
+                session_unset();
+                session_destroy();
+                header("location: ../pages/login.php");
+            }
+        } else {
+            header("location: ../pages/users.php");
+        }
+    } else {
+        header("location: ../pages/login.php");
+    }
+?>
