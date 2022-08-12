570210731db6f6c22473f1c5950378d59d914159
jersonsilvaa <jerson.silva99@hotmail.com>
2022-08-11 21:40:40 -0500

    #39 [Quitando texto de relleno de prueba]

--- pages/chat.php
+++ pages/chat.php
@@ -28,50 +28,7 @@
                 </div>
             </header>
             <div class="chat-box">
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Grefg una pregunta, cuando me llega el pc? estoy muy emocionado</p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="https://esports.as.com/2020/01/07/Grefg.png?hash=c1c7a3a13b4e5bbc9db2295d1f67110ae61425fe" alt="">
-                    <div class="details">
-                        <p>Claro bro. te mando ese pc el lunes</p>
-                    </div>
-                </div>
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Ese pc me servirá para juegos triple A?</p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="https://esports.as.com/2020/01/07/Grefg.png?hash=c1c7a3a13b4e5bbc9db2295d1f67110ae61425fe" alt="">
-                    <div class="details">
-                        <p>Como lo había mencionado, es un pc tocho bro</p>
-                    </div>
-                </div>
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Una pregunta, en un video se vió un consolador de tu novia, es real?</p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="https://esports.as.com/2020/01/07/Grefg.png?hash=c1c7a3a13b4e5bbc9db2295d1f67110ae61425fe" alt="">
-                    <div class="details">
-                        <p>De hecho se colocó ahí adrede, sí fuera real, se hubiera cortado esa parte</p>
-                    </div>
-                </div>
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Entiendo, muchas gracias grefg, estaré esperando con ansias. Mandaré una foto cuando llega o incluso un video</p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="https://esports.as.com/2020/01/07/Grefg.png?hash=c1c7a3a13b4e5bbc9db2295d1f67110ae61425fe" alt="">
-                    <div class="details">
-                        <p>Te lo agradecería bro, para que la gente crea que es real</p>
-                    </div>
-                </div>
+             
             </div>
             <form action="#" class="typing-area" autocomplete="off">
                 <input type="text" name="outgoing_id" value="<?php echo $_SESSION['unique_id']; ?>" hidden>
